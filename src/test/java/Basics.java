9f744b80ee110d3613b3f85754708263d47cd05d
Faisal_1 <faisal.m.arafeh@gmail.com>
2025-10-28 18:10:43 +0300

    new code part-2

--- src/test/java/Basics.java
+++ src/test/java/Basics.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -39,20 +40,19 @@ public class Basics {
 //}
 
 
-
     public static void main(String[] args) throws InterruptedException {
-    WebDriver driver = new FirefoxDriver();
+        WebDriver driver = new FirefoxDriver();
 
-    driver.get("https://www.facebook.com");
-    System.out.println("Page Title: " + driver.getTitle());
-    Thread.sleep(5000);
-    driver.quit();
-}
+        driver.get("https://www.facebook.com");
+        System.out.println("Page Title: " + driver.getTitle());
+        Thread.sleep(5000);
+        driver.quit();
+    }
 
-   WebDriver driver = new ChromeDriver();
+    WebDriver driver = new ChromeDriver();
 
     @Test
-     public void BrowserCommands () throws InterruptedException {
+    public void BrowserCommands() throws InterruptedException {
         driver.get("https://google.com");
         TimeUnit.SECONDS.sleep(3);
 
@@ -72,7 +72,7 @@ public class Basics {
     }
 
     @Test
-     public void MaximizeWindow () throws InterruptedException {
+    public void MaximizeWindow() throws InterruptedException {
         driver.get("https://www.linkedin.com/");
         Thread.sleep(3000);
         driver.manage().window().maximize();
@@ -81,7 +81,7 @@ public class Basics {
     }
 
     @Test
-     public void BrowserOptions () throws InterruptedException {
+    public void BrowserOptions() throws InterruptedException {
         ChromeOptions options = new ChromeOptions();
 
         options.addArguments("--start-maximized");
@@ -92,12 +92,12 @@ public class Basics {
 
         WebDriver driver = new ChromeDriver(options);
         driver.get("https://www.linkedin.com/");
-        Thread.sleep(30000);
+        Thread.sleep(3000);
         driver.quit();
     }
 
     @Test
-     public void BrowserOptionsHeadless () throws InterruptedException {
+    public void BrowserOptionsHeadless() throws InterruptedException {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless=new");
         WebDriver driver = new ChromeDriver(options);
@@ -105,10 +105,10 @@ public class Basics {
         System.out.println("Page Title: " + driver.getTitle());
         Thread.sleep(5000);
         driver.quit();
-      }
+    }
 
     @Test
-    public void loginTest () throws InterruptedException {
+    public void loginTest() throws InterruptedException {
         driver.get("https://www.globalsqa.com/angularJs-protractor/registration-login-example/#/login");
         Thread.sleep(3000);// Bad Code
 
@@ -126,60 +126,58 @@ public class Basics {
                 "//div[contains(@class,'ng-binding ng-scope')]")).getText();
 
         Thread.sleep(3000);
-        System.out.println("Error Message: "+ errorMessage);
+        System.out.println("Error Message: " + errorMessage);
         Thread.sleep(3000);
 
         driver.quit();
     }
 
-@Test
-     public void js () throws InterruptedException {
-    driver.get("https://www.globalsqa.com/angularJs-protractor/registration-login-example/#/login");
-    Thread.sleep(2000);
-
-    WebElement username = driver.findElement(By.id("username"));
-    WebElement pass = driver.findElement(By.name("password"));
-    WebElement loginButton = driver.findElement(By.tagName("button"));
-
-    JavascriptExecutor js = (JavascriptExecutor) driver;
-
-    js.executeScript("arguments[0].value = arguments[1];", username, "Faisal_UserName");
-    js.executeScript("arguments[0].value = arguments[1];", pass, "MyPassword");
-
-    js.executeScript("arguments[0].removeAttribute('disabled');", loginButton);
-    js.executeScript("arguments[0].click();", loginButton);
-
-    Thread.sleep(5000);
-    driver.quit();
-}
-
-@Test
-     public void Action () throws InterruptedException {
-    driver.get("https://www.globalsqa.com/angularJs-protractor/registration-login-example/#/login");
-    Thread.sleep(2000);
-
-    WebElement username = driver.findElement(By.id("username"));
-    WebElement pass = driver.findElement(By.name("password"));
-    WebElement loginButton = driver.findElement(By.tagName("button"));
-
-    Actions actions = new Actions(driver);
-
-    actions.sendKeys(username, "Faisal").perform();
-    actions.sendKeys(pass, "MyPass").perform();
-    actions.click(loginButton).perform();
-
-    Thread.sleep(5000);
-
-
-    driver.quit();
-}
-
-
-    //  WebElement pass = driver.findElement(By.name("password"));
-    //WebElement loginButton = driver.findElement(By.tagName("button"));
 
     @Test
-    public void RelativeLocator () throws InterruptedException {
+    public void js() throws InterruptedException {
+        driver.get("https://www.globalsqa.com/angularJs-protractor/registration-login-example/#/login");
+        Thread.sleep(2000);
+
+        WebElement username = driver.findElement(By.id("username"));
+        WebElement pass = driver.findElement(By.name("password"));
+        WebElement loginButton = driver.findElement(By.tagName("button"));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        js.executeScript("arguments[0].value = arguments[1];", username, "Faisal_UserName");
+        js.executeScript("arguments[0].value = arguments[1];", pass, "MyPassword");
+
+        js.executeScript("arguments[0].removeAttribute('disabled');", loginButton);
+        js.executeScript("arguments[0].click();", loginButton);
+
+        Thread.sleep(5000);
+        driver.quit();
+    }
+
+    @Test
+    public void Action() throws InterruptedException {
+        driver.get("https://www.globalsqa.com/angularJs-protractor/registration-login-example/#/login");
+        Thread.sleep(2000);
+
+        WebElement username = driver.findElement(By.id("username"));
+        WebElement pass = driver.findElement(By.name("password"));
+        WebElement loginButton = driver.findElement(By.tagName("button"));
+
+        Actions actions = new Actions(driver);
+
+        actions.sendKeys(username, "Faisal").perform();
+        actions.sendKeys(pass, "MyPass").perform();
+        actions.click(loginButton).perform();
+
+        Thread.sleep(5000);
+
+
+        driver.quit();
+    }
+
+
+    @Test
+    public void RelativeLocator() throws InterruptedException {
         driver.get("https://www.globalsqa.com/angularJs-protractor/registration-login-example/#/login");
         Thread.sleep(2000);
 
@@ -192,28 +190,43 @@ public class Basics {
 
         //WebElement loginButton = driver.findElement(By.tagName("button"));
 
-        Thread.sleep(50000);
+        Thread.sleep(5000);
 
         driver.quit();
     }
 
-
     @Test
-    public void RelativeLocator_2 () throws InterruptedException {
+    public void RelativeLocator_2() throws InterruptedException {
         driver.get("https://www.globalsqa.com/angularJs-protractor/registration-login-example/#/login");
         Thread.sleep(2000);
 
         WebElement password = driver.findElement(By.id("password"));
+        password.sendKeys("myPassword");
 
         WebElement userName = driver.findElement(with(By.tagName("input")).above(password));
         userName.sendKeys("Above Password");
 
-        // WebElement loginButton = driver.findElement(By.tagName("button"));
 
-        Thread.sleep(50000);
 
-        driver.quit(); // Faisal
+
+        Thread.sleep(5000);
+
+
+        driver.quit(); // Faisal new update
     }
 
 
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
